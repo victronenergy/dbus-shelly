victronenergy/dbus-shelly

commit 65b5fd94c470abd004b3eddb004484609f5f7454

Add channel name path
Add a new path `/Devices/<MAC>/<n>/Name` which holds the user-defined channel name, if set.
If the user has not specified a name, the path will be invalid.

This path is used to show the channel name in the shelly integrations menu.

https://github.com/victronenergy/gui-v2/issues/3050

shelly_device.py
@@ -340,18 +340,20 @@ class ShellyDevice(object):
 
 	# Get the number of switching and/or metering channels.
 	async def _get_channels_info(self):
-		channels = []
+		channels = {}
 
 		async def add_channels(ch_type, capabilities):
 			ch = 0
 			while True:
 				found = False
 				for cap in (c for c in capabilities if c in self._capabilities):
-					resp = await self.rpc_call(f"{cap}.GetStatus", {"id": ch})
+					resp = await self.rpc_call(f"{cap}.GetConfig", {"id": ch})
 					if resp is None:
 						continue
 					found = True
-					channels.append(f"{ch_type}_{ch}")
+					name = resp.get("name", None)
+					index = len(channels)	# The discovery service relies on a unique incremental index for each channel
+					channels[index] = {"type": ch_type, "name": name, "id": f'{ch_type}_{ch}'}
 					ch += 1
 				if not found:
 					# No capabilities on this channel number, or the channel doesn't exist at all. Done.
@@ -387,7 +389,9 @@ class ShellyDevice(object):
 				logger.error(f"Invalid channel {channel} for shelly device {self.serial_or_server}, which has channels: {self._channel_info}")
 				return False
 
-			is_switch = 'switch' in channel
+			ch_type = self._channel_info[channel]['type']
+			ch_type_id = self._channel_info[channel]['id']
+			is_switch = ch_type == 'switch'
 			name = f"Shelly {'Switch' if is_switch else 'EM'}"
 			id = PRODUCT_ID_SHELLY_SWITCH if is_switch else PRODUCT_ID_SHELLY_EM
 
@@ -395,7 +399,7 @@ class ShellyDevice(object):
 			ch = await ShellyChannel.create(
 				bus_type=self._bus_type,
 				serial=self._serial,
-				channel_type_id=channel,
+				channel_type_id=ch_type_id,
 				productid=id,
 				productName=name,
 				server=self._shelly_device.ip_address or self._server,
@@ -405,7 +409,7 @@ class ShellyDevice(object):
 			# Create handlers for the generic + switching OR EM capabilities
 			handlers = {}
 			for cap in self._capabilities:
-				cls = shelly_handlers.get_handler_class(cap, channel.split('_')[0])
+				cls = shelly_handlers.get_handler_class(cap, ch_type)
 				if cls is None:
 					continue
 				create_new = True
@@ -426,7 +430,7 @@ class ShellyDevice(object):
 				
 			await ch.start_service()
 
-			self._channels[channel] = {"channel": ch, "handlers": handlers}
+			self._channels[channel] = {"channel": ch, "handlers": handlers, "ch_num": ch_type_id.split('_')[-1]}
 			return True
 
 	async def restart_channel(self, channel):
@@ -492,8 +496,8 @@ class ShellyDevice(object):
 	def device_updated(self, cb_device, update_type):
 		if update_type == RpcUpdateType.STATUS:
 			for ch in self._channels.keys():
-				channel = ch.split('_')[1]
 				entry = self._channels[ch]
+				channel = entry.get("ch_num")
 				handlers = entry.get("handlers", {})
 				for cap, handler in handlers.items():
 					cap = cap.lower()

discovery.py
@@ -479,28 +479,32 @@ class ShellyManager(object):
 		if known:
 			return
 
-		for i, ch in enumerate(channel_info):
-			ch_type = ch.split('_')[0] # 'switch', 'em'
+		for i, ch_prop in channel_info.items():
+			ch_type = ch_prop['type']
+			ch_name = ch_prop['name']
 
 			# Don't encode the channel type in the setting path to remain compatible with older versions.
 			# There are two types of channels: 'switch' and 'em'. Switch channels are enumerated first, then em channels.
 			# Note, settings are stored 0-indexed, while the channels on dbus are 1 indexed.
-			await self.settings.add_settings(Setting(f'/Settings/Devices/shelly_{serial}/{i}/Enabled', 0, alias=f"enabled_{serial}_{ch}"))
-			enabled = self.settings.get_value(self.settings.alias(f"enabled_{serial}_{ch}"))
+			await self.settings.add_settings(Setting(f'/Settings/Devices/shelly_{serial}/{i}/Enabled', 0, alias=f"enabled_{serial}_{i}"))
+			enabled = self.settings.get_value(self.settings.alias(f"enabled_{serial}_{i}"))
 
 			if self.service.get_item(f'/Devices/{serial}/{i + 1}/Enabled') is None:
 				self.service.add_item(IntegerItem(f'/Devices/{serial}/{i + 1}/Enabled',
-									  writeable=True, onchange=partial(self._on_enabled_changed, serial, ch)))
+									  writeable=True, onchange=partial(self._on_enabled_changed, serial, i)))
 			if self.service.get_item(f'/Devices/{serial}/{i + 1}/Type') is None:
 				self.service.add_item(TextItem(f'/Devices/{serial}/{i + 1}/Type', writeable=False))
+			if self.service.get_item(f'/Devices/{serial}/{i + 1}/Name') is None:
+				self.service.add_item(TextItem(f'/Devices/{serial}/{i + 1}/Name', value='', writeable=False))
 
 			with self.service as s:
 				s[f'/Devices/{serial}/{i + 1}/Type'] = ch_type
 				s[f'/Devices/{serial}/{i + 1}/Enabled'] = enabled
+				s[f'/Devices/{serial}/{i + 1}/Name'] = ch_name
 
 			if enabled:
 				enabled_item = self.service.get_item(f'/Devices/{serial}/{i + 1}/Enabled')
-				await self._on_enabled_changed(serial, ch, enabled_item, enabled)
+				await self._on_enabled_changed(serial, i, enabled_item, enabled)
 
 	async def _on_enabled_changed(self, serial, channel, item, value):
 		if value not in (0, 1) or item.service is None:

commit 1010f64d74a78d885769d9340197b20647c42aba

Fix channel reinitialization
This bug was introduced in commit 65b5fd94c470abd004b3eddb004484609f5f7454

shelly_device.py
@@ -319,6 +319,7 @@ class ShellyDevice(object):
 		# Check if capabilities changed
 		cap_changed = False
 		handlers = self._channels[ch].get("handlers", {})
+		ch_type = self._channels[ch].get("ch_type", None)
 		# Capability removed
 		for cap in list(handlers.keys()):
 			if cap not in self._capabilities:
@@ -326,7 +327,7 @@ class ShellyDevice(object):
 				break
 		# Capability added for which we have a handler
 		for cap in self._capabilities:
-			if cap not in handlers and shelly_handlers.get_handler_class(cap, ch.split('_')[0]) is not None:
+			if cap not in handlers and shelly_handlers.get_handler_class(cap, ch_type) is not None:
 				cap_changed = True
 				break
 
@@ -453,7 +454,7 @@ class ShellyDevice(object):
 						pass
 				return False
 
-			self._channels[channel] = {"channel": ch, "handlers": handlers, "ch_num": ch_type_id.split('_')[-1]}
+			self._channels[channel] = {"channel": ch, "handlers": handlers, "ch_type": ch_type, "ch_num": ch_type_id.split('_')[-1]}
 			return True
 
 	async def restart_channel(self, channel):